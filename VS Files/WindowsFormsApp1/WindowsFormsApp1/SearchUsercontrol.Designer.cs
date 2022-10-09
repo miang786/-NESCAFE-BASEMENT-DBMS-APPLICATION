f1570f7941798d30d664f329d80afca51ce33595
MIAN AFZAAL ZAHOOR <61027154+miang786@users.noreply.github.com>
2022-10-09 17:57:26 +0500

    Add files via upload

---
+++ VS Files/WindowsFormsApp1/WindowsFormsApp1/SearchUsercontrol.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class SearchUsercontrol
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 235);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(816, 199);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(418, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Brown;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(459, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PATIENT NUMBER";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Fuchsia;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(302, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 57);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "SEARCH";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Fuchsia;
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(571, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 57);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "PLASMA DONOR LIST";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.pic;
+            this.pictureBox1.Location = new System.Drawing.Point(7, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(214, 208);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(396, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 29);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "ISLAMABAD DOCTORS HOSPITAL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "MIAN AFZAAL ZAHOOR";
+            // 
+            // SearchUsercontrol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.doctor;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "SearchUsercontrol";
+            this.Size = new System.Drawing.Size(835, 437);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ VS Files/WindowsFormsApp1/WindowsFormsApp1/SearchUsercontrol.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace WindowsFormsApp1
+{
+    public partial class SearchUsercontrol : UserControl
+    {
+        SqlConnection con = new SqlConnection(@"Data Source = DESKTOP-HHFFIIR;Initial Catalog = DBMS_LAB FINAL; Integrated Security = True");
+        public SearchUsercontrol()
+        {
+            InitializeComponent();
+            
+        }
+        public void Dis_Data()
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select Recovered_Patient.MR_no as PATIENT_NUMBER, Register_Patient_Details.P_First_Name as PATIENT NAME, Register_Patient_Details.P_Last_Name as LAST_NAME, Register_Patient_Details.P_Father_Name as FATHER_NAME, Register_Patient_Details.P_City as CITY, Register_Patient_Details.P_Blood_Group as BLOOD_GROUP, Recovered_Patient.D_Name as DOCTOR, Recovered_Patient.Date_Of_Adm as ADMISSION_DATE, Recovered_Patient.Date_Of_Dis as DISCHARGE_DATE, Recovered_Patient.Willing_For_Plasma_Donation as PLASMA_WILLING from Recovered_Patient inner join Register_Patient_Details on Register_Patient_Details.MR_no = Recovered_Patient.MR_no where MR.no = '" + textBox1.Text + "' "; 
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Dis_Data();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dis_Data();
+        }
+        public void Dis_Data1()
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select Recovered_Patient.MR_no as PATIENT_NUMBER, Register_Patient_Details.P_First_Name as PATIENT NAME, Register_Patient_Details.P_Last_Name as LAST_NAME, Register_Patient_Details.P_Father_Name as FATHER_NAME, Register_Patient_Details.P_City as CITY, Register_Patient_Details.P_Blood_Group as BLOOD_GROUP, Recovered_Patient.D_Name as DOCTOR, Recovered_Patient.Date_Of_Adm as ADMISSION_DATE, Recovered_Patient.Date_Of_Dis as DISCHARGE_DATE, Recovered_Patient.Willing_For_Plasma_Donation as PLASMA_WILLING from Recovered_Patient inner join Register_Patient_Details on Register_Patient_Details.MR_no = Recovered_Patient.MR_no where Willing_For_Plasma_Donation = '1';";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Dis_Data1();
+        }
+    }
+}
